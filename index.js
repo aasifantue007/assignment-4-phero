2bbb69b3137b711ab62f6228c64a444b9df55a7a
Aasif Antue <aasifantue@Aasifs-Mac-mini.local>
2023-02-07 20:11:10 +0600

    4 done

---
+++ index.js
@@ -0,0 +1,118 @@
+// Problem - 01 // 
+
+// Input - 1 - 5 //
+
+// function mindGame(number){
+
+//     const multi = number * 3;
+//     const add   = multi + 10;
+//     const divison = add / 2;
+//     const sub     = divison - 5;
+//     return sub;
+
+// }
+
+// const mynumber = 5;
+// const finalResult = mindGame(mynumber);
+
+// console.log(finalResult);
+
+
+// Input - 2 - 50 //
+
+// function mindGame(number){
+
+//     const multi = number * 3;
+//     const add   = multi + 10;
+//     const divison = add / 2;
+//     const sub     = divison - 5;
+//     return sub;
+
+// }
+
+// const mynumber = 50;
+// const finalResult = mindGame(mynumber);
+
+// console.log(finalResult);
+
+
+// Input - 3 - 33 //
+
+// function mindGame(number){
+
+//     const multi = number * 3;
+//     const add   = multi + 10;
+//     const divison = add / 2;
+//     const sub     = divison - 5;
+//     return sub;
+
+// }
+
+// const mynumber = 33;
+// const finalResult = mindGame(mynumber);
+
+// console.log(finalResult);
+
+
+
+
+// Problem - 02 // 
+
+// function evenOdd(words){
+    
+
+//     for (let i = 0; i < words.length; i++ ){
+//         if (words[i].length % 2 === 0) {
+//             console.log("even");
+//         }
+//         else {
+//             console.log("odd");
+//         }
+//     }
+// }
+
+// let textOutput = ["phero", "batch7", "chatgpt"];
+
+// evenOdd(textOutput);
+
+
+// Problem - 03 // 
+
+
+
+
+// function isLGSeven(numbers){
+
+//     let result = numbers - 7;
+
+//      if(result < 7){
+//             console.log(result)
+//      }
+
+//      else{
+//             console.log(numbers*2);
+//     }
+// }
+  
+// isLGSeven(6);
+// isLGSeven(-15);
+// isLGSeven(15);
+
+
+// Problem - 04 // 
+// function findingBadData(numbers){
+//     let count = 0;
+
+// for (let i = 0; i <numbers.length; i++){
+//     if(numbers[i] < 0){
+//         count++
+//     }
+// }
+//     return count;
+// }
+// // let numbers = [1, 2, 5];
+// // let numbers = [ 2, -5, -7, -13 ];
+// let numbers = [ -4, -9, -5, -33, -55 ];
+// let negativeCount = findingBadData(numbers);
+
+// console.log(negativeCount);
